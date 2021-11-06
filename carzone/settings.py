4e9310150acafeb42185dc788bf07c2dd2be9c3e
Fernando Hdez <ferhd@yahoo.com>
2021-11-05 18:16:27 -0600

    Completed Teams Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,14 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'carzoneDB',
+        'USER': 'postgres',
+        #'PASSWORD': 'qWerty@123',
+        'PASSWORD': 'Cw95hubXEx',
+        'HOST': 'localhost',
     }
 }
 
@@ -122,6 +129,11 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'carzone/static',
 ]
+
+# Media files
+
+MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Team
+from django.utils.html import format_html
 
 # Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html(f'<img src="{object.photo.url}" width="40px" style="border-radius: 50px" />')
+
+    thumbnail.short_description = 'photo'
+    list_display = ('id', 'thumbnail', 'first_name', 'last_name', 'designation', 'created_date',)
+    list_display_links = ('id', 'thumbnail', 'first_name',)
+    search_fields = ('first_name', 'last_name', 'designation', 'created_date',)
+    list_filter = ('designation', 'created_date',)
+
+admin.site.register(Team, TeamAdmin)
